eb7c09f180af235e2c264b9591afffab166dfde9
lollobianco <loreb2002@gmail.com>
2022-12-15 15:31:33 +0100

    Ristrutturata posizione cartelle e fatto axios

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once __DIR__ . '/../db/index.php';
+
+$arrayDischi = [];
+
+foreach( $database as $elem ){
+
+  $arrayDischi[] = $elem;
+
+}
+
+header('Content-type: application/json');
+
+echo json_encode( $arrayDischi );
+
+?>
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,21 @@ var app = new Vue({
 el: '#app',
 data: {
 
+   arrayDischi : [],
+
 },
+
 mounted() {
+
+   axios.get('./api/index.php')
+
+      .then((res) => {
+
+         this.arrayDischi = res.data
+
+         console.log(this.arrayDischi)
+
+      })
 
 },
 created() {
